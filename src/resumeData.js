e7570c94983de543da5e59f655f0f97edbe472b2
Favour Adesiyan <Favouradesiyan2@gmail.com>
2022-11-22 21:18:00 -0330

    updated src and public folder

---
+++ src/resumeData.js
@@ -0,0 +1,86 @@
+let resumeData = {
+  name: "Favour Adesiyan",
+  role: "Frontend Developer and Data Scientist",
+  linkedinId: "https://www.linkedin.com/in/favour-adesiyan-a41384203/",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/favour-adesiyan-a41384203/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/r",
+      className: "fa fa-github",
+    },
+    { id: "3", title: "Email", link: "mailto:favouradesiyan2@gmail.com" },
+  ],
+  aboutme:
+    "I am an Ontario-based future web developer. I have a strong enthusiasm for web development and am hoping to learn about UI effects, animations, and creating engaging, dynamic user interfaces. Organizer, problem solver, and self-starter with great attention to detail. Fan of anime, music, outdoor activities, and language learning. I like working with optimistic, ambitious people.",
+  address: "Toronto, Ontario",
+  education: [
+    {
+      UniversityName: "Memorial university ,Computer engineering",
+      MonthOfPassing: "may",
+      YearOfPassing: "2025",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Student",
+      specialization: "Software development",
+      MonthOfLeaving: "September 2022",
+      YearOfLeaving: "",
+      Achievements: "",
+    },
+  ],
+  skillsDescription:
+    "Full stack developer,Which is a person who can develop both client and server software.",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
+
+export default resumeData;
